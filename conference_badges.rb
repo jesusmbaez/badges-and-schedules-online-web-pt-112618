cb2395a775c3af9751faa44b823307af814ab013
Manny Baez <jesusmbaez@gmail.com>
2018-12-08 22:10:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+
+
+def batch_badge_creator(attendees)
+  badges = [] 
+  attendees.each {|person| badges.push("Hello, my name is #{person}.")}
+  badges
+end 
+ 
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index{|speaker, index| rooms.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")}
+  rooms
+end 
+ 
+def printer(attendees)
+  
+end 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
